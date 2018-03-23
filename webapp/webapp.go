37001f177a0a885cbd5f51246935175a0e092e65
fain182 <fain182@gmail.com>
2018-03-23 16:36:30 +0100

    Use react to populate page

--- webapp/webapp.go
+++ webapp/webapp.go
@@ -17,10 +17,6 @@ func Serve(records archive.RecordArchive) {
 	homepageHandlerWithAuthentication := httpauth.SimpleBasicAuth(os.Getenv("USER"), os.Getenv("PASSWORD"))(homepageHandler)
 	http.Handle("/", homepageHandlerWithAuthentication)
 
-	apiRecordingsHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { apiRecordings(records, w, r) })
-	apiRecordingsHandlerWithAuthentication := httpauth.SimpleBasicAuth(os.Getenv("USER"), os.Getenv("PASSWORD"))(apiRecordingsHandler)
-	http.Handle("/recordings.json", apiRecordingsHandlerWithAuthentication)
-
 	println("Listening on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
@@ -31,16 +27,12 @@ func homepage(records archive.RecordArchive, w http.ResponseWriter, r *http.Requ
 		panic(err)
 	}
 
-	t.Execute(w, archive.SortRecords(records))
-}
-
-func apiRecordings(records archive.RecordArchive, w http.ResponseWriter, r *http.Request) {
-	output, err := json.MarshalIndent(archive.SortRecords(records), "", "\t")
+	jsonRecordings, err := json.Marshal(archive.SortRecords(records))
 	if err != nil {
 		println(err)
+	} else {
+		t.Execute(w, template.JS(string(jsonRecordings)))
 	}
-
-	w.Write(output)
 }
 
 func getAssetsPath() string {
