351a8022288d2aae25f810fdf1316ca00d3f4521
fain182 <fain182@gmail.com>
2018-03-23 15:36:48 +0100

    Adds API for recordings

--- webapp/webapp.go
+++ webapp/webapp.go
@@ -1,6 +1,7 @@
 package webapp
 
 import (
+	"encoding/json"
 	"html/template"
 	"log"
 	"net/http"
@@ -12,9 +13,14 @@ import (
 )
 
 func Serve(records archive.RecordArchive) {
-	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { homepage(records, w, r) })
-	handlerWithAuthentication := httpauth.SimpleBasicAuth(os.Getenv("USER"), os.Getenv("PASSWORD"))(handler)
-	http.Handle("/", handlerWithAuthentication)
+	homepageHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { homepage(records, w, r) })
+	homepageHandlerWithAuthentication := httpauth.SimpleBasicAuth(os.Getenv("USER"), os.Getenv("PASSWORD"))(homepageHandler)
+	http.Handle("/", homepageHandlerWithAuthentication)
+
+	apiRecordingsHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { apiRecordings(records, w, r) })
+	apiRecordingsHandlerWithAuthentication := httpauth.SimpleBasicAuth(os.Getenv("USER"), os.Getenv("PASSWORD"))(apiRecordingsHandler)
+	http.Handle("/recordings.json", apiRecordingsHandlerWithAuthentication)
+
 	println("Listening on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
@@ -26,6 +32,15 @@ func homepage(records archive.RecordArchive, w http.ResponseWriter, r *http.Requ
 	}
 
 	t.Execute(w, archive.SortRecords(records))
+}
+
+func apiRecordings(records archive.RecordArchive, w http.ResponseWriter, r *http.Request) {
+	output, err := json.MarshalIndent(archive.SortRecords(records), "", "\t")
+	if err != nil {
+		println(err)
+	}
+
+	w.Write(output)
 }
 
 func getAssetsPath() string {
