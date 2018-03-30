4bde4fdae01572bc1b5697225257d5456ecdf2ab
fain182 <fain182@gmail.com>
2018-03-30 15:25:58 +0200

    Refactored tests, using table driven test

--- archive/RecordArchive_test.go
+++ archive/RecordArchive_test.go
@@ -35,30 +35,30 @@ func TestAddOneMessageToRecordings(t *testing.T) {
 	}
 }
 
-func TestUploadedSimpleFile(t *testing.T) {
-	title := getTitle("uploaded a file: <https://ideato.slack.com/files/U1KD1QEJ1/F9K5MT0PN/zoom_0.mp4|Registrazione incontro 06/03>")
-	if title != "Registrazione incontro 06/03" {
-		t.Error("Expected 'canale', got ", title)
+func TestTitleParsing(t *testing.T) {
+	cases := []struct{ Body, ExpectedTitle string }{
+		{
+			"uploaded a file: <https://ideato.slack.com/files/U1KD1QEJ1/F9K5MT0PN/zoom_0.mp4|Registrazione incontro 06/03>",
+			"Registrazione incontro 06/03",
+		},
+		{
+			"uploaded a file: <https://ideato.slack.com/files/U024HSKKW/F8YHNGHCZ/zoom_0.mp4|zoom_0.mp4> and commented: @channel ecco la registrazione del lean coffee",
+			"ecco la registrazione del lean coffee",
+		},
+		{
+			"uploaded a file: <https://ideato.slack.com/files/U024HSKKW/F8YHNGHCZ/zoom_0.mp4|zoom_0.mp4>",
+			"zoom 0",
+		},
+		{
+			"uploaded a file: <https://ideato.slack.com/files/U024HSKKW/F8YHNGHCZ/common_language.m4a|common_language.m4a>",
+			"common language",
+		},
 	}
-}
 
-func TestDefaultZoomFilenameWithComment(t *testing.T) {
-	title := getTitle("uploaded a file: <https://ideato.slack.com/files/U024HSKKW/F8YHNGHCZ/zoom_0.mp4|zoom_0.mp4> and commented: @channel ecco la registrazione del lean coffee")
-	if title != "ecco la registrazione del lean coffee" {
-		t.Error("Expected 'ecco la registrazione del lean coffee', got ", title)
-	}
-}
-
-func TestDefaultZoomFilenameWithoutComment(t *testing.T) {
-	title := getTitle("uploaded a file: <https://ideato.slack.com/files/U024HSKKW/F8YHNGHCZ/zoom_0.mp4|zoom_0.mp4>")
-	if title != "zoom 0" {
-		t.Error("Expected 'zoom 0', got ", title)
-	}
-}
-
-func TestNameWithUnderscoreAndFileExtension(t *testing.T) {
-	title := getTitle("uploaded a file: <https://ideato.slack.com/files/U024HSKKW/F8YHNGHCZ/common_language.m4a|common_language.m4a>")
-	if title != "common language" {
-		t.Error("Expected 'common language', got ", title)
+	for _, c := range cases {
+		actualTitle := getTitle(c.Body)
+		if actualTitle != c.ExpectedTitle {
+			t.Error("Expected '", c.ExpectedTitle, "', got ", actualTitle)
+		}
 	}
 }
